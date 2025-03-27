ee4fb17212c393ad8b27749e418802bea3741039
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-03-27 13:33:23 +0530

    finished image uploade process

--- Task-4 Create an image upload and retrieval api/script.js
+++ Task-4 Create an image upload and retrieval api/script.js
@@ -1,1 +1,54 @@
-console.log("hello world ");
+
+const formField=document.querySelector('.form-container');
+const uploadedArea=document.querySelector('.img-upload-field');
+const fileInput=document.querySelector('.img-upload-input');
+// Timestamp
+const timestampDisplay=document.querySelector('.timestampdisplay');
+
+
+
+// Uploade Image Click 
+function uploadProcess(){
+
+    uploadedArea.addEventListener('click',()=>{
+    fileInput.click();
+
+})
+
+}
+
+uploadProcess()
+
+
+
+// Timestamp Display process 
+function timeStampBtn(){
+
+    formField.addEventListener('submit',(event)=>{
+
+        event.preventDefault()
+
+
+    const formattedDate=new Date();
+   
+    
+    
+    timestampDisplay.innerHTML = `<b>Uploaded Time</b>: ${formattedDate}`;
+
+
+    // alert the submition and rest from 
+
+    setTimeout(()=>{
+        alert('Image uploade successfully!');
+        myForm.reset(); 
+    },500);
+    
+      
+    })
+
+
+
+}
+
+timeStampBtn()
+
